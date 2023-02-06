53884506c4bfc62b20c7ee08f486be3329db1bca
iimyzf <agnaou004@gmail.com>
2023-02-06 16:06:15 +0100

    modified

--- src/constants/fetchFromAPI.js
+++ src/constants/fetchFromAPI.js
@@ -1,6 +1,6 @@
 import axios from "axios";
+import "./config";
 
-const RAPID_API_KEY = "f78f2c7ad1mshc8f2794407dc272p12bc84jsn8ee38d8b3523";
 const BASE_URL = "https://youtube-v31.p.rapidapi.com";
 
 const options = {
@@ -8,7 +8,7 @@ const options = {
         maxResults: "50",
     },
     headers: {
-        "X-RapidAPI-Key": RAPID_API_KEY,
+        "X-RapidAPI-Key": API_KEY,
         "X-RapidAPI-Host": "youtube-v31.p.rapidapi.com",
     },
 };
